e78daba57aea1d0bde2d4e921ecbd2ef62b5fdbc
rogerball <rojjball@gmail.com>
2021-07-16 17:07:20 +0100

    remove unnecessary dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,18 +7,16 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "4.2.0",
-    "uk.gov.hmrc"             %% "simple-reactivemongo"       % "8.0.0-play-28",
-    "uk.gov.hmrc"             %% "mongo-lock"                 % mongoLock
+    "uk.gov.hmrc"             %% "simple-reactivemongo"       % "8.0.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.3.0"             ,
-    
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "4.2.0"             % Test,
+
     "org.scalatest"           %% "scalatest"                  % "3.2.5"             % Test,
     "com.typesafe.play"       %% "play-test"                  % PlayVersion.current % Test,
     "uk.gov.hmrc"                %% "reactivemongo-test"      % "5.0.0-play-28"     % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"             % "test, it",
-    "uk.gov.hmrc"                %% "auth-client"             % "5.5.0-play-28"
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"             % "test, it"
   )
 }
