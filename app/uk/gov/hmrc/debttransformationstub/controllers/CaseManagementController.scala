48def14b4a16c9918f0d9a251a947c01060165d4
Divilioglu <mo.divilioglu@gmail.com>
2021-12-09 09:33:13 +0000

    [DTD-764] - Added Case Management Create Case end point

---
+++ app/uk/gov/hmrc/debttransformationstub/controllers/CaseManagementController.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.debttransformationstub.controllers
+import java.io.File
+
+import javax.inject.Inject
+import play.api.Environment
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, ControllerComponents, Request}
+import uk.gov.hmrc.debttransformationstub.config.AppConfig
+import uk.gov.hmrc.debttransformationstub.models.casemanagement.CreateCaseRequest
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import scala.concurrent.Future
+import scala.io.Source
+
+class CaseManagementController @Inject()(environment: Environment,
+                                         cc: ControllerComponents,
+                                         appConfig: AppConfig)
+    extends BackendController(cc)
+    with BaseController {
+  private val basePath = "conf/resources/data"
+
+  def createCase: Action[JsValue] = Action.async(parse.json) {
+    implicit request: Request[JsValue] =>
+      {
+        withCustomJsonBody[CreateCaseRequest] { req =>
+          val fileMaybe: Option[File] = environment.getExistingFile(
+            s"$basePath/casemanagement.createcase/create-case-response.json"
+          )
+
+          fileMaybe match {
+            case None => Future successful NotFound("file not found")
+            case Some(file) =>
+              val result = Source.fromFile(file).mkString.stripMargin
+              Future successful Ok(result)
+          }
+
+        }
+      }
+  }
+}
