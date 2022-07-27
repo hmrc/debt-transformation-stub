fdc9337d5fa86fcec6ee8ed8bd908870da9af0d6
TevynAllen <allen.tevyn@gmail.com>
2022-07-27 11:36:18 +0100

    Update hmrc mongo dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.3.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.64.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.68.0",
     "com.beachape"      %% "enumeratum-play-json"      % "1.6.1"
   )
 
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.3.0"  % Test,
     "org.mockito"            %% "mockito-scala-scalatest" % "1.17.5" % Test,
     "com.typesafe.play"      %% "play-test"               % "2.8.8"  % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.64.0" % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.68.0" % Test,
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.36.8" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"  % "test, it"
   )
