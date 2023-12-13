d810026c766b906c162b6094efb1a6ff75ace770
sainahjan <141128938+sainahjan@users.noreply.github.com>
2023-12-13 09:44:56 +0000

    DTD-2115: Update dependencies again, and removed unused ones.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "8.1.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "8.2.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "1.6.0",
     "com.beachape"      %% "enumeratum-play-json"      % "1.8.0",
     "commons-io"        %  "commons-io"                % "2.11.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % "8.1.0"   % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % "8.2.0"   % Test,
     "org.mockito"            %% "mockito-scala-scalatest" % "1.17.30" % Test,
-    "com.typesafe.play"      %% "play-test"               % "2.8.18"  % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % "1.6.0"   % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8"  % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"   % Test
   )
 }
