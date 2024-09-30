a34a8c25840bd74cc2d4ded18e25fa1455cc65ab
paige.dellafranca <paige.dellafranca@digital.hmrc.gov.uk>
2024-09-30 14:42:19 +0100

    DTD-2721 Update stub with mandatory fields for ETMP VATC/PAYE

--- app/uk/gov/hmrc/debttransformationstub/controllers/ETMPController.scala
+++ app/uk/gov/hmrc/debttransformationstub/controllers/ETMPController.scala
@@ -41,7 +41,7 @@ class ETMPController @Inject() (environment: Environment, cc: ControllerComponen
   def paymentPlanEligibility(regimeType: String, idType: String, idValue: String): Action[AnyContent] = Action {
     implicit request: Request[AnyContent] =>
       val queryKeys: List[String] =
-        List("showIds", "showAddresses", "showSignals", "showFiling", "showCharges", "addressFromDate")
+        List("showIds", "showAddresses", "showSignals", "showFiling", "showCharges", "addressFromDate","showAdditionalCustData")
       val queries: Map[String, Option[String]] = queryKeys.map(key => (key, request.getQueryString(key))).toMap
       queries("showIds")
       val relativePath = s"$basePath" + "." + regimeType + "/" + s"$idValue.json"
