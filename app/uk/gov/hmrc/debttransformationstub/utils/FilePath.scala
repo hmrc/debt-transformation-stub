b1f8be9c06b0b017894d90dee662316270ed74ba
anthos.kountouris <anthos.kountouris@gmail.com>
2024-11-07 12:46:24 +0000

    DTD-2881: Makes relativePath dynamic to accept all idValues

---
+++ app/uk/gov/hmrc/debttransformationstub/utils/FilePath.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.debttransformationstub.utils
+
+import java.io.File
+
+object FilePath {
+
+  def getFilePath(basePath: String, idValue: String): String = {
+    val directory = new File(basePath)
+
+    var fileName = ""
+    if (directory.exists() && directory.isDirectory) {
+      val files = directory.listFiles()
+      if (files != null) {
+        for (file <- files)
+          if (file.getName.contains(idValue)) {
+            fileName = file.getName
+          }
+      }
+    }
+    s"$basePath" + "/" + s"$fileName"
+  }
+
+}
