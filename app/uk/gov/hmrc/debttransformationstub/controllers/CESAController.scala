23177bda0c27b97c6506dbd57807435f308811f9
paigedellafranca <paige.dellafranca@digital.hmrc.gov.uk>
2025-05-27 15:52:53 +0100

    DTD-2231 "Revised with review changes"

--- app/uk/gov/hmrc/debttransformationstub/controllers/CESAController.scala
+++ app/uk/gov/hmrc/debttransformationstub/controllers/CESAController.scala
@@ -17,10 +17,10 @@
 package uk.gov.hmrc.debttransformationstub.controllers
 
 import play.api.Environment
-import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
-import play.api.mvc.{Action, ControllerComponents, Request}
+import play.api.libs.json.{ JsError, JsSuccess, JsValue, Json }
+import play.api.mvc.{ Action, ControllerComponents, Request }
 import uk.gov.hmrc.debttransformationstub.models.errors.NO_RESPONSE
-import uk.gov.hmrc.debttransformationstub.models.{CesaDataRequest, CesaDataResponse, CustomerDataRequest, Identity}
+import uk.gov.hmrc.debttransformationstub.models.{ CesaDataRequest, CesaDataResponse, CustomerDataRequest, Identity }
 import uk.gov.hmrc.debttransformationstub.utils.RequestAwareLogger
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
@@ -30,7 +30,7 @@ import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 import scala.concurrent.Future
 import scala.io.Source
-import scala.util.{Failure, Success, Try, Using}
+import scala.util.{ Failure, Success, Try, Using }
 
 class CESAController @Inject() (environment: Environment, cc: ControllerComponents)
     extends BackendController(cc) with CustomBaseController {
@@ -68,93 +68,4 @@ class CESAController @Inject() (environment: Environment, cc: ControllerComponen
     }
   }
 
-  def saCustomerData(): Action[JsValue] = Action(parse.json) { implicit request =>
-    request.body.validate[CustomerDataRequest] match {
-      case JsError(errors) =>
-        BadRequest(s"Unable to parse to CustomerDataRequest: $errors")
-      case JsSuccess(value, _) =>
-        val fileName: String = value.identifications
-          .getOrElse(List.empty[Identity])
-          .find { case Identity(idType, _) => idType == "UTR" }
-          .map(_.idValue)
-          .get
-
-        if (fileName.isEmpty) {
-          NotFound("IdValue for UTR not provided")
-        } else {
-          val relativePath = s"$basePath" + "/" + s"$fileName.json"
-          environment.getExistingFile(relativePath) match {
-            case Some(file) =>
-              Try(Json.parse(saCustomerDataString(file))) match {
-                case Success(value) => Ok(value)
-                case Failure(exception) =>
-                  logger.error(s"Failed to parse the file $relativePath", exception)
-                  InternalServerError(s"stub failed to parse file $relativePath")
-              }
-            case _ =>
-              NotFound("file not found")
-          }
-        }
-    }
-  }
-
-  private def saCustomerDataString(file: File): String = {
-    val currentDate = LocalDate.now()
-
-    val responseTemplate: String =
-      Using(Source.fromFile(file))(source => source.mkString).recoverWith { case ex: Throwable =>
-        // Explain which file failed to be read.
-        Failure(new RuntimeException(s"Failed to read file: ${file.getPath}", ex))
-      }.get // Can throw.
-
-    val dueDateInPast = currentDate.minusDays(24)
-    val dueDateToday = currentDate
-    val dueDateInFuture = currentDate.plusDays(24)
-
-    val result =
-      responseTemplate
-        .replaceAll("<DUE_DATE>", dueDateInPast.format(dateFormatter))
-        .replaceAll("<DUE_DATE_TODAY>", dueDateToday.format(dateFormatter))
-        .replaceAll("<DUE_DATE_FOR_FUTURE>", dueDateInFuture.format(dateFormatter))
-
-    println(
-      s"""====================
-         |$result
-         |====================
-         |""".stripMargin
-    )
-    result
-  }
-
-  def cesaData(): Action[JsValue] = Action.async(parse.json) { implicit rawRequest: Request[JsValue] =>
-    withCustomJsonBody[CesaDataResponse] { request =>
-      val fileName = s"$basePath.CesaDataResponse/${request.chargeReference}.json"
-      environment.getExistingFile(fileName) match {
-        case None =>
-          val message = s"file [$fileName] not found"
-          logger.error(s"Status $NOT_FOUND, message: $message")
-          Future successful NotFound(message)
-        case Some(file) =>
-          val maybeFileContent: Try[String] =
-            Using(Source.fromFile(file))(source => source.mkString)
-              .map { responseBody =>
-                logger.info(s"""Responding from CESA with body from file $file :\n$responseBody""")
-                responseBody
-              }
-              .recoverWith { case ex: Throwable =>
-                // Explain which file failed to be read.
-                Failure(new RuntimeException(s"Failed to read file: ${file.getPath}", ex))
-              }
-
-          maybeFileContent match {
-            case Success(value) =>
-              // Might throw if parsing fails
-              Future.successful(Ok(Json.parse(value)))
-            case Failure(exception) =>
-              logger.error(s"Failed to parse the file $file", exception)
-              Future.successful(InternalServerError(s"Stub failed to parse file $file"))
-          }
-      }
-    }
-  }
 }
