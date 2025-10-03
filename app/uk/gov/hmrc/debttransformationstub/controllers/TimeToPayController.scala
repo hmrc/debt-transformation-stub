f55ca59783494fe88bd8eac7d5d3fe69394b97d7
rogerball <rojjball@gmail.com>
2025-10-03 12:21:36 +0100

    DTD-3877 add missing stub files

--- app/uk/gov/hmrc/debttransformationstub/controllers/TimeToPayController.scala
+++ app/uk/gov/hmrc/debttransformationstub/controllers/TimeToPayController.scala
@@ -17,14 +17,13 @@
 package uk.gov.hmrc.debttransformationstub.controllers
 
 import org.apache.commons.io.FileUtils
-import org.apache.pekko.stream.Materializer
 import play.api.Environment
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{ JsValue, Json }
 import play.api.mvc._
 import uk.gov.hmrc.debttransformationstub.config.AppConfig
-import uk.gov.hmrc.debttransformationstub.models.CdcsCreateCaseRequestWrappedTypes.{CdcsCreateCaseRequestIdTypeReference, CdcsCreateCaseRequestLastName}
+import uk.gov.hmrc.debttransformationstub.models.CdcsCreateCaseRequestWrappedTypes.{ CdcsCreateCaseRequestIdTypeReference, CdcsCreateCaseRequestLastName }
 import uk.gov.hmrc.debttransformationstub.models._
-import uk.gov.hmrc.debttransformationstub.repositories.{EnactStage, EnactStageRepository}
+import uk.gov.hmrc.debttransformationstub.repositories.{ EnactStage, EnactStageRepository }
 import uk.gov.hmrc.debttransformationstub.services.TTPPollingService
 import uk.gov.hmrc.debttransformationstub.utils.RequestAwareLogger
 import uk.gov.hmrc.http.HeaderCarrier
@@ -33,7 +32,7 @@ import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import java.io.File
 import java.nio.charset.Charset
 import javax.inject.Inject
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.io.Source
 import scala.util.Try
 
@@ -43,7 +42,7 @@ class TimeToPayController @Inject() (
   appConfig: AppConfig,
   ttpPollingService: TTPPollingService,
   enactStageRepository: EnactStageRepository
-)(implicit ec: ExecutionContext, materializer: Materializer)
+)(implicit ec: ExecutionContext)
     extends BackendController(cc) with CustomBaseController {
 
   private lazy val logger = new RequestAwareLogger(this.getClass)
@@ -221,17 +220,6 @@ class TimeToPayController @Inject() (
       for {
         _            <- enactStageRepository.addETMPStage(correlationId, req)
         fileResponse <- constructResponse(s"/etmp.executePaymentLock/", s"${req.idValue}.json")
-        responseEntity <- fileResponse.body.consumeData.map(_.utf8String)
-        _ = logger.info(
-          s"""Returning ETMP response:
-             |=====================
-             |${fileResponse.header.status} ${fileResponse.header.reasonPhrase}
-             |${fileResponse.header.headers}
-             |
-             |$responseEntity
-             |=====================
-             |""".stripMargin)
-
       } yield fileResponse
     }
   }
