2cdc5986608ed08bc221cf88217cf2d813930d5a
witchelljack <36855484+WitchellJack@users.noreply.github.com>
2025-11-03 14:30:11 +0000

    DTD-4013 - amended UTR for CDCS call

--- app/uk/gov/hmrc/debttransformationstub/controllers/TimeToPayController.scala
+++ app/uk/gov/hmrc/debttransformationstub/controllers/TimeToPayController.scala
@@ -304,7 +304,7 @@ class TimeToPayController @Inject() (
               x
             } else {
               identifier match {
-                case "3145760528" => Some(Status(INTERNAL_SERVER_ERROR))
+                case "3153830017" => Some(Status(INTERNAL_SERVER_ERROR))
                 case s if s.startsWith("cdcsResponse_error_") =>
                   val code = s.stripPrefix("cdcsResponse_error_").takeWhile(_.isDigit)
                   Some(Status(code.toInt))
