5165fa4f3b591c6c2cb95f88b6997c4583080109
jamgoNC <239327899+jamgoNC@users.noreply.github.com>
2025-11-21 14:42:37 +0000

    DTD-4068: Updated simple dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,15 +10,15 @@ object AppDependencies {
     "com.fasterxml.jackson.core" % "jackson-core"              % "2.19.2",
     "uk.gov.hmrc"               %% "bootstrap-backend-play-30" % bootstrapPlay,
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"        % hmrcMongoPlay,
-    "com.beachape"              %% "enumeratum-play-json"      % "1.8.2",
-    "commons-io"                 % "commons-io"                % "2.19.0",
+    "com.beachape"              %% "enumeratum-play-json"      % "1.9.0",
+    "commons-io"                 % "commons-io"                % "2.21.0",
     "com.nrinaudo"              %% "kantan.csv"                % "0.8.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapPlay % Test,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.17.37"     % Test,
+    "org.mockito"            %% "mockito-scala-scalatest" % "2.0.0"     % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongoPlay % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1"       % Test
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2"       % Test
   )
 }
